d4a2ff35ea9ddcc317cc61f346b68ab77bbf7c59
李威 <liweijsj@163.com>
2023-02-16 10:03:32 +0800

    自定义 laravel-doctrine 表名前缀

--- laravel-doctrine-example-app/config/doctrine.php
+++ laravel-doctrine-example-app/config/doctrine.php
@@ -88,7 +88,7 @@ return [
     |
     */
     'extensions'                 => [
-        //LaravelDoctrine\ORM\Extensions\TablePrefix\TablePrefixExtension::class,
+        LaravelDoctrine\ORM\Extensions\TablePrefix\TablePrefixExtension::class,
         //LaravelDoctrine\Extensions\Timestamps\TimestampableExtension::class,
         //LaravelDoctrine\Extensions\SoftDeletes\SoftDeleteableExtension::class,
         //LaravelDoctrine\Extensions\Sluggable\SluggableExtension::class,
